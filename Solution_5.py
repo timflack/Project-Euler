19641c95172648d02d479a44b68ea96944fbfd9b
timflack <42137731+timflack@users.noreply.github.com>
2019-05-01 10:36:21 +0100

    Update Solution_5.py

--- Solution_5.py
+++ Solution_5.py
@@ -6,10 +6,11 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 check = [11, 13, 14, 16, 17, 18, 19, 20]
 
-def scope(start, stop, step, check):
+def solution_5(start, stop, step, check):
     for a in range(start, stop, step):
         if all(a % checks == 0 for checks in check):
             return a
             
-x = scope(2520, 999999999, 2, check)
-print(x)
+start_time = time.time()
+solution_5(2520, 999999999, 2, check)
+print("--- %s seconds ---" % (time.time() - start_time))
